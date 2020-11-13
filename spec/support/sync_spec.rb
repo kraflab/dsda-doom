a0e6f04f6a8a9ce0795b778f01154aaef8165cc1
kraflab <kraflab@gmail.com>
2020-11-14 00:15:42 +0100

    Add cl 9 and cl 11 sync specs

--- spec/support/sync_spec.rb
+++ spec/support/sync_spec.rb
@@ -8,10 +8,27 @@ RSpec.describe 'sync' do
   describe 'total time' do
     subject { Utility.read_levelstat.total }
     
+    # complevel 2
     context 'doom2 30uv in 17:55 by Looper' do
       let(:lmp) { '30uv1755.lmp' }
       
       it { is_expected.to eq('17:55') }
+    end
+    
+    # complevel 9
+    context 'rush 12 uv max in 21:14 by Ancalagon' do
+      let(:lmp) { 'ru12-2114.lmp' }
+      let(:pwad) { 'rush.wad' }
+      
+      it { is_expected.to eq('21:14') }
+    end
+    
+    # complevel 11
+    context 'valiant e1 uv speed in 5:13 by Krankdud' do
+      let(:lmp) { 'vae1-513.lmp' }
+      let(:pwad) { 'Valiant.wad' }
+      
+      it { is_expected.to eq('5:13') }
     end
   end
 end
