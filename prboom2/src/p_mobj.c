e31ca9caa1fc83250807756506f41fd57aa73862
kraflab <kraflab@gmail.com>
2021-01-03 19:03:52 +0100

    Adjust spawnmobj checks

--- prboom2/src/p_mobj.c
+++ prboom2/src/p_mobj.c
@@ -1127,8 +1127,8 @@ mobj_t* P_SpawnMobj(fixed_t x,fixed_t y,fixed_t z,mobjtype_t type)
   mobj->radius = info->radius;
   mobj->height = info->height;                                      // phares
   mobj->flags  = info->flags;
-  if (heretic) mobj->flags2 = info->flags2;
-  if (heretic) mobj->damage = info->damage;
+  mobj->flags2 = info->flags2;
+  if (heretic) mobj->damage = info->damage; // HERETIC_TODO: doom doesn't do this, but why?
 
   /* killough 8/23/98: no friends, bouncers, or touchy things in old demos */
   if (!mbf_features)
