d6380027ddb7c81eb14938eb4c833cb516ea5742
kraflab <kraflab@gmail.com>
2021-01-07 19:02:04 +0100

    Remove redundant definitions

--- prboom2/src/heretic/_p_local.h
+++ prboom2/src/heretic/_p_local.h
@@ -23,28 +23,6 @@
 #include "r_local.h"
 #endif
 
-// ***** P_MAP *****
-
-extern boolean floatok;         // if true, move would be ok if
-extern fixed_t tmfloorz, tmceilingz;    // within tmfloorz - tmceilingz
-
-extern line_t *ceilingline;
-boolean P_CheckPosition(mobj_t * thing, fixed_t x, fixed_t y);
-boolean P_TryMove(mobj_t * thing, fixed_t x, fixed_t y);
-boolean P_TeleportMove(mobj_t * thing, fixed_t x, fixed_t y);
-void P_SlideMove(mobj_t * mo);
-void P_UseLines(player_t * player);
-
-boolean P_ChangeSector(sector_t * sector, boolean crunch);
-
-extern mobj_t *linetarget;      // who got hit (or NULL)
-fixed_t P_AimLineAttack(mobj_t * t1, angle_t angle, fixed_t distance);
-
-void P_LineAttack(mobj_t * t1, angle_t angle, fixed_t distance, fixed_t slope,
-                  int damage);
-
-void P_RadiusAttack(mobj_t * spot, mobj_t * source, int damage);
-
 // ***** P_SETUP *****
 
 extern byte *rejectmatrix;      // for fast sight rejection
