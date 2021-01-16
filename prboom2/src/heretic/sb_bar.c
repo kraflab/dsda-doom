f9371f357576b8237effa8b3c49ea695fcd655a9
kraflab <kraflab@gmail.com>
2021-01-16 19:27:41 +0100

    Stub sb_bar

---
+++ prboom2/src/heretic/sb_bar.c
@@ -0,0 +1,41 @@
+//
+// Copyright(C) 1993-1996 Id Software, Inc.
+// Copyright(C) 1993-2008 Raven Software
+// Copyright(C) 2005-2014 Simon Howard
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+
+// SB_bar.c
+
+#include "sb_bar.h"
+
+// Temporary stubs
+
+void SB_Ticker(void)
+{
+
+}
+
+void SB_Drawer(void)
+{
+
+}
+
+void SB_Init(void)
+{
+
+}
+
+dboolean SB_Responder(event_t *ev)
+{
+  return false;
+}
