43236cf65ca1eb86ed29fd59f836aa903f92659d
kraflab <kraflab@gmail.com>
2021-03-11 08:40:42 +0100

    Nullify mouse activation position

--- prboom2/src/SDL/i_video.c
+++ prboom2/src/SDL/i_video.c
@@ -1411,6 +1411,7 @@ void I_UpdateVideoMode(void)
 static void ActivateMouse(void)
 {
   SDL_SetRelativeMouseMode(SDL_TRUE);
+  SDL_GetRelativeMouseState(NULL, NULL);
 }
 
 static void DeactivateMouse(void)
