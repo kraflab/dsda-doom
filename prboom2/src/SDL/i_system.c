82dce1b8b245bf75ab963d73a4c20b0fef28b121
kraflab <kraflab@gmail.com>
2021-03-19 21:48:00 +0100

    Improve GetTime precision

--- prboom2/src/SDL/i_system.c
+++ prboom2/src/SDL/i_system.c
@@ -106,20 +106,24 @@ void I_uSleep(unsigned long usecs)
 
 int ms_to_next_tick;
 
-static int basetime = 0;
 int I_GetTime_RealTime (void)
 {
+  static dboolean started = false;
   int i;
-  int t = SDL_GetTicks();
+  unsigned long long t;
 
   //e6y: removing startup delay
-  if (basetime == 0)
-    basetime = t;
-  t -= basetime;
+  if (!started)
+  {
+    started = true;
+    dsda_StartTimer(dsda_timer_realtime);
+  }
 
-  i = t*(TICRATE/5)/200;
-  ms_to_next_tick = (i+1)*200/(TICRATE/5) - t;
+  t = dsda_ElapsedTime(dsda_timer_realtime);
+
   if (ms_to_next_tick > 1000/TICRATE || ms_to_next_tick<1) ms_to_next_tick = 1;
+  i = t * TICRATE / 1000000;
+  ms_to_next_tick = (i + 1) * 1000 / TICRATE - t / 1000;
   return i;
 }
 
