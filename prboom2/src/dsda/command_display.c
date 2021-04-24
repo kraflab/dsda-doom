0167d10f5f6ba550a8367207747f5f44f5b32b75
kraflab <kraflab@gmail.com>
2021-04-25 00:21:15 +0200

    Correct text

--- prboom2/src/dsda/command_display.c
+++ prboom2/src/dsda/command_display.c
@@ -12,7 +12,7 @@
 // GNU General Public License for more details.
 //
 // DESCRIPTION:
-//	DSDA Input Display
+//	DSDA Command Display
 //
 
 #include "st_stuff.h"
--- prboom2/src/dsda/command_display.h
+++ prboom2/src/dsda/command_display.h
@@ -12,7 +12,7 @@
 // GNU General Public License for more details.
 //
 // DESCRIPTION:
-//	DSDA Input Display
+//	DSDA Command Display
 //
 
 #ifndef __DSDA_COMMAND_DISPLAY__
