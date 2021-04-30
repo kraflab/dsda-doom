sultim-t/prboom-plus-rt

Fix for UMAPINFO music restarting issue. (#281)
* Fix for UMAPINFO music restarting issue.

* Fix bugs with MUSINFO and UMAPINFO present simultaneously.

Fixes #279, fixes #102.

prboom2/src/s_advsound.c
@@ -59,15 +59,27 @@ musinfo_t musinfo;
 //
 void S_ParseMusInfo(const char *mapid)
 {
-  memset(&musinfo, 0, sizeof(musinfo));
-  musinfo.current_item = -1;
-
-  S_music[NUMMUSIC].lumpnum = -1;
 
   if (W_CheckNumForName("MUSINFO") != -1)
   {
     int num, lumpnum;
     int inMap = false;
+    int load_muslump = -1;
+    /* musinfo item zero is initialized
+     * before we reach the parser and it must be
+     * saved and restored */
+    int itemzero = musinfo.items[0];
+
+    /* don't restart music that is already playing */
+    if (mus_playing &&
+        mus_playing->lumpnum == S_music[NUMMUSIC].lumpnum) {
+        load_muslump = S_music[NUMMUSIC].lumpnum;
+    }
+
+    memset(&musinfo, 0, sizeof(musinfo));
+    musinfo.items[0] = itemzero;
+    musinfo.current_item = load_muslump;
+    S_music[NUMMUSIC].lumpnum = load_muslump;
 
     SC_OpenLump("MUSINFO");
 

kraflab/dsda-doom

prboom2/src/s_advsound.c
@@ -59,15 +59,26 @@ musinfo_t musinfo;
 //
 void S_ParseMusInfo(const char *mapid)
 {
-  memset(&musinfo, 0, sizeof(musinfo));
-  musinfo.current_item = -1;
-
-  S_music[num_music].lumpnum = -1;
-
   if (W_CheckNumForName("MUSINFO") != -1)
   {
     int num, lumpnum;
     int inMap = false;
+    int load_muslump = -1;
+    /* musinfo item zero is initialized
+     * before we reach the parser and it must be
+     * saved and restored */
+    int itemzero = musinfo.items[0];
+
+    /* don't restart music that is already playing */
+    if (mus_playing &&
+        mus_playing->lumpnum == S_music[num_music].lumpnum) {
+        load_muslump = S_music[num_music].lumpnum;
+    }
+
+    memset(&musinfo, 0, sizeof(musinfo));
+    musinfo.items[0] = itemzero;
+    musinfo.current_item = load_muslump;
+    S_music[num_music].lumpnum = load_muslump;
 
     SC_OpenLump("MUSINFO");
 