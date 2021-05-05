683fc7213bd64985216d4cf052cf65b63a164317
kraflab <kraflab@gmail.com>
2021-05-05 21:59:06 +0200

    Simplify splash group feature

--- prboom2/src/d_deh.c
+++ prboom2/src/d_deh.c
@@ -1237,7 +1237,6 @@ static const struct deh_flag_s deh_mobjflags_mbf21[] = {
   {"E3M8BOSS",       MF2_E3M8BOSS}, // E3M8 boss
   {"E4M6BOSS",       MF2_E4M6BOSS}, // E4M6 boss
   {"E4M8BOSS",       MF2_E4M8BOSS}, // E4M8 boss
-  {"NEUTRALSPLASH",  MF2_NEUTRALSPLASH}, // splash damage ignores splash groups
   {"RIP",            MF2_RIP}, // projectile rips through targets
   { NULL }
 };
