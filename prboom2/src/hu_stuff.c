368de14774d65af9f66b6b343c2352a298dde33b
Roman Fomin <rfomin@gmail.com>
2021-06-30 16:28:52 +0700

    UMAPINFO: entries without defined 'levelname' fall back to default (#339)

--- prboom2/src/hu_stuff.c
+++ prboom2/src/hu_stuff.c
@@ -788,7 +788,7 @@ void HU_Start(void)
     &message_list
   );
 
-  if (gamemapinfo != NULL)
+  if (gamemapinfo && gamemapinfo->levelname)
   {
 	  if (gamemapinfo->label)
 		  s = gamemapinfo->label;
@@ -803,7 +803,6 @@ void HU_Start(void)
 		  HUlib_addCharToTextLine(&w_title, ' ');
 	  }
 	  s = gamemapinfo->levelname;
-	  if (!s) s = "Unnamed";
   }
   else
   {
