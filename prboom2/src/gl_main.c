ed236e21c1130c1ae4c085f72bcd72760c19e38f
kraflab <kraflab@gmail.com>
2021-08-13 11:21:55 +0200

    Reduce gl log noise

--- prboom2/src/gl_main.c
+++ prboom2/src/gl_main.c
@@ -352,7 +352,7 @@ void gld_Init(int width, int height)
   lprintf(LO_INFO,"GL_VENDOR: %s\n",glGetString(GL_VENDOR));
   lprintf(LO_INFO,"GL_RENDERER: %s\n",glGetString(GL_RENDERER));
   lprintf(LO_INFO,"GL_VERSION: %s\n",glGetString(GL_VERSION));
-  lprintf(LO_INFO,"GL_EXTENSIONS:\n");
+  lprintf(LO_DEBUG,"GL_EXTENSIONS:\n");
   {
     char ext_name[256];
     const char *extensions = (const char*)glGetString(GL_EXTENSIONS);
@@ -373,7 +373,7 @@ void gld_Init(int width, int height)
         len = MIN(len, sizeof(ext_name)-1);
         memset(ext_name, 0, sizeof(ext_name));
         strncpy(ext_name, rover, len);
-        lprintf(LO_INFO,"\t%s\n", ext_name);
+        lprintf(LO_DEBUG,"\t%s\n", ext_name);
       }
       rover = p;
       while (*rover && *rover == ' ')
