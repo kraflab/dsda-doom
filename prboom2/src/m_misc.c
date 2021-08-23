07a77f0271804a215b27ee6c056f9e48a5286f5d
kraflab <kraflab@gmail.com>
2021-08-23 18:46:14 +0200

    Set default skill level uv

--- prboom2/src/m_misc.c
+++ prboom2/src/m_misc.c
@@ -328,7 +328,7 @@ default_t defaults[] =
   {"dehfile_2",{NULL,&deh_files[1]},{0,""},UL,UL,def_str,ss_none},
 
   {"Game settings",{NULL},{0},UL,UL,def_none,ss_none},
-  {"default_skill",{&defaultskill},{3},1,5, // jff 3/24/98 allow default skill setting
+  {"default_skill",{&defaultskill},{4},1,5, // jff 3/24/98 allow default skill setting
    def_int,ss_none}, // selects default skill 1=TYTD 2=NTR 3=HMP 4=UV 5=NM
   {"player_bobbing",{&default_player_bobbing},{1},0,1,         // phares 2/25/98
    def_bool,ss_weap, &player_bobbing},
