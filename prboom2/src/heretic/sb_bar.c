4087bf2172e85500cf57ea4290fcb309826e8832
kraflab <kraflab@gmail.com>
2021-12-07 20:58:01 +0100

    Fix heretic playback pause

--- prboom2/src/heretic/sb_bar.c
+++ prboom2/src/heretic/sb_bar.c
@@ -317,7 +317,7 @@ void SB_Ticker(void)
     int delta;
     int curHealth;
 
-    if (heretic && leveltime & 1 && !paused_via_menu)
+    if (heretic && leveltime & 1 && !paused_outside_demo)
     {
         ChainWiggle = P_Random(pr_heretic) & 1;
     }
