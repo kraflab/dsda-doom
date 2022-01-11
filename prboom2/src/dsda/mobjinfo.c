fa7c600529edfd7e92f6b1a6a288cf957e884982
kraflab <kraflab@gmail.com>
2022-01-11 22:20:24 +0100

    Manage dehacked thing indices

--- prboom2/src/dsda/mobjinfo.c
+++ prboom2/src/dsda/mobjinfo.c
@@ -59,6 +59,46 @@ static void dsda_EnsureCapacity(int limit) {
   }
 }
 
+typedef struct deh_mobj_index_entry_s {
+  int index_in;
+  int index_out;
+  struct deh_mobj_index_entry_s* next;
+} deh_mobj_index_entry_t;
+
+#define DEH_MOBJ_INDEX_HASH_SIZE 128
+
+static deh_mobj_index_entry_t deh_mobj_index_hash[DEH_MOBJ_INDEX_HASH_SIZE];
+
+static int deh_mobj_index_start;
+static int deh_mobj_index_end;
+
+static int dsda_GetDehMobjIndex(int index) {
+  deh_mobj_index_entry_t* entry;
+
+  if (index < deh_mobj_index_start)
+    return index;
+
+  entry = &deh_mobj_index_hash[index % DEH_MOBJ_INDEX_HASH_SIZE];
+
+  while (entry->next && entry->index_in != index)
+    entry = entry->next;
+
+  if (entry->index_in != index) {
+    entry->next = calloc(1, sizeof(*entry));
+    entry = entry->next;
+    entry->index_in = index;
+    entry->index_out = deh_mobj_index_end;
+    ++deh_mobj_index_end;
+  }
+
+  return entry->index_out;
+}
+
+// Dehacked has the index off by 1
+int dsda_TranslateDehMobjIndex(int index) {
+  return dsda_GetDehMobjIndex(index - 1) + 1;
+}
+
 dsda_deh_mobjinfo_t dsda_GetDehMobjInfo(int index) {
   dsda_deh_mobjinfo_t deh_mobjinfo;
 
@@ -80,6 +120,8 @@ void dsda_InitializeMobjInfo(int zero, int max, int count) {
 
   if (raven) return;
 
+  deh_mobj_index_start = num_mobj_types;
+  deh_mobj_index_end = num_mobj_types;
   edited_mobjinfo_bits = calloc(num_mobj_types, sizeof(*edited_mobjinfo_bits));
 }
 
