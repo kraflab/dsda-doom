da560c2bd58f2f2fdf98d2e78a30373c96933c09
kraflab <kraflab@gmail.com>
2022-04-23 14:51:14 +0200

    Fix dsda_SplitString calloc

--- prboom2/src/dsda/utility.c
+++ prboom2/src/dsda/utility.c
@@ -29,7 +29,7 @@ char** dsda_SplitString(char* str, const char* delimiter) {
     if (*p++ == *delimiter)
       ++substring_count;
 
-  result = calloc(substring_count, sizeof(*str));
+  result = calloc(substring_count, sizeof(*result));
 
   if (result) {
     char* token;
