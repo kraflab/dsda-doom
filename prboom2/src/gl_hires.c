664953bfb959fa8ba8e3d232d139fcdd39a10e6d
kraflab <kraflab@gmail.com>
2022-07-11 00:52:00 +0200

    Resolve RGB2PAL

--- prboom2/src/gl_hires.c
+++ prboom2/src/gl_hires.c
@@ -830,7 +830,6 @@ void gld_HiRes_ProcessColormap(unsigned char *buffer, int bufSize)
 
 int gld_HiRes_BuildTables(void)
 {
-#define RGB2PAL_NAME "RGB2PAL"
   const int chanel_bits = (gl_hires_24bit_colormap ? 8 : 5);
   const int numcolors_per_chanel = (1 << chanel_bits);
   const int RGB2PAL_size = numcolors_per_chanel * numcolors_per_chanel * numcolors_per_chanel;
@@ -845,7 +844,7 @@ int gld_HiRes_BuildTables(void)
 
   if (gl_hires_24bit_colormap)
   {
-    lump = W_CheckNumForName(RGB2PAL_NAME);
+    lump = W_CheckNumForName("RGB2PAL");
     if (lump != -1)
     {
       size = W_LumpLength(lump);
@@ -860,7 +859,7 @@ int gld_HiRes_BuildTables(void)
       }
     }
 
-    RGB2PAL_fname = I_FindFile(RGB2PAL_NAME".dat", ".dat");
+    RGB2PAL_fname = I_FindFile("RGB2PAL.dat", ".dat");
     if (RGB2PAL_fname)
     {
       struct stat RGB24to8_stat;
@@ -878,7 +877,6 @@ int gld_HiRes_BuildTables(void)
     }
   }
 
-  if (1 || M_CheckParm("-"RGB2PAL_NAME))
   {
     int ok = true;
     FILE *RGB2PAL_fp = NULL;
@@ -886,7 +884,7 @@ int gld_HiRes_BuildTables(void)
 
     if (gl_hires_24bit_colormap)
     {
-      doom_snprintf(fname, sizeof(fname), "%s/"RGB2PAL_NAME".dat", I_DoomExeDir());
+      doom_snprintf(fname, sizeof(fname), "%s/RGB2PAL.dat", I_DoomExeDir());
       RGB2PAL_fp = fopen(fname, "wb");
       ok = RGB2PAL_fp != NULL;
     }
