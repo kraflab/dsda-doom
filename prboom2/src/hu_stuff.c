0369a1b2c02492d0117b3b04075106057626b6a6
kraflab <kraflab@gmail.com>
2022-08-20 12:33:59 +0200

    Move shift form to menu

--- prboom2/src/hu_stuff.c
+++ prboom2/src/hu_stuff.c
@@ -188,56 +188,9 @@ static char hud_monsecstr[80];
 extern int map_point_coordinates;
 extern int map_level_stat;
 
-// key tables
-// jff 5/10/98 french support removed,
-// as it was not being used and couldn't be easily tested
-//
-const char* shiftxform;
-
 static custom_message_t custom_message[MAX_MAXPLAYERS];
 static custom_message_t *custom_message_p;
 void HU_init_crosshair(void);
-
-const char english_shiftxform[] =
-{
-  0,
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-  11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-  21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-  31,
-  ' ', '!', '"', '#', '$', '%', '&',
-  '"', // shift-'
-  '(', ')', '*', '+',
-  '<', // shift-,
-  '_', // shift--
-  '>', // shift-.
-  '?', // shift-/
-  ')', // shift-0
-  '!', // shift-1
-  '@', // shift-2
-  '#', // shift-3
-  '$', // shift-4
-  '%', // shift-5
-  '^', // shift-6
-  '&', // shift-7
-  '*', // shift-8
-  '(', // shift-9
-  ':',
-  ':', // shift-;
-  '<',
-  '+', // shift-=
-  '>', '?', '@',
-  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-  'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-  '[', // shift-[
-  '!', // shift-backslash - OH MY GOD DOES WATCOM SUCK
-  ']', // shift-]
-  '"', '_',
-  '\'', // shift-`
-  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-  'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-  '{', '|', '}', '~', 127
-};
 
 static void HU_SetLumpTrans(const char *name)
 {
@@ -266,8 +219,6 @@ void HU_Init(void)
   int   i;
   int   j;
   char  buffer[9];
-
-  shiftxform = english_shiftxform;
 
   // load the heads-up font
   j = HU_FONTSTART;
--- prboom2/src/m_menu.c
+++ prboom2/src/m_menu.c
@@ -184,7 +184,6 @@ int mapcolor_me;    // cph
 extern int map_point_coordinates; // killough 10/98
 extern int map_level_stat;
 
-extern const char* shiftxform;
 extern default_t defaults[];
 extern int numdefaults;
 
@@ -287,6 +286,47 @@ void M_ChangeApplyPalette(void);
 menu_t NewDef;                                              // phares 5/04/98
 
 // end of prototypes added to support Setup Menus and Extended HELP screens
+
+const char shiftxform[] =
+{
+  0,
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+  11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+  31,
+  ' ', '!', '"', '#', '$', '%', '&',
+  '"', // shift-'
+  '(', ')', '*', '+',
+  '<', // shift-,
+  '_', // shift--
+  '>', // shift-.
+  '?', // shift-/
+  ')', // shift-0
+  '!', // shift-1
+  '@', // shift-2
+  '#', // shift-3
+  '$', // shift-4
+  '%', // shift-5
+  '^', // shift-6
+  '&', // shift-7
+  '*', // shift-8
+  '(', // shift-9
+  ':',
+  ':', // shift-;
+  '<',
+  '+', // shift-=
+  '>', '?', '@',
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+  'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+  '[', // shift-[
+  '!', // shift-backslash - OH MY GOD DOES WATCOM SUCK
+  ']', // shift-]
+  '"', '_',
+  '\'', // shift-`
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+  'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+  '{', '|', '}', '~', 127
+};
 
 /////////////////////////////////////////////////////////////////////////////
 //
