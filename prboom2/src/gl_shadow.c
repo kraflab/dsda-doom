fccdaefc8b676a689494e6dc1f70d7adfb81ef1d
kraflab <kraflab@gmail.com>
2022-09-26 18:26:58 +0200

    Revert obsolete fix for gl shadows

--- prboom2/src/gl_shadow.c
+++ prboom2/src/gl_shadow.c
@@ -250,7 +250,6 @@ void gld_RenderShadows(void)
   gl_EnableFog(false);
 
   glDepthMask(GL_FALSE);
-  glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_FALSE);
   glBlendFunc(GL_ZERO, GL_ONE_MINUS_SRC_COLOR);
 
   // Apply a modelview shift.
@@ -274,6 +273,5 @@ void gld_RenderShadows(void)
 
   glPopMatrix();
   glDepthMask(GL_TRUE);
-  glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
