16fd970afc0752c87d343b2a19f830734e3bf1f8
Xaser Acheron <xaser@angryscience.net>
2022-09-25 19:22:00 -0500

    fix incorrect NUM_GAMMA_LEVELS constant (welp :P)

--- prboom2/src/v_video.h
+++ prboom2/src/v_video.h
@@ -114,7 +114,7 @@ extern int          usegamma;
 #define PALETTE_SIZE 768
 
 // [XA] wonder why this was never a constant... silly id. :P
-#define NUM_GAMMA_LEVELS 4
+#define NUM_GAMMA_LEVELS 5
 
 // The available bit-depth modes
 typedef enum {
