27c3d0c663a8c3b17f8b2735206a1df99ab3c79b
kraflab <kraflab@gmail.com>
2022-10-17 16:25:38 +0200

    Move default visibility into component definition

--- prboom2/src/dsda/exhud.c
+++ prboom2/src/dsda/exhud.c
@@ -41,6 +41,7 @@ typedef struct {
   const char* name;
   int default_vpt;
   dboolean strict;
+  dboolean off_by_default;
   dboolean on;
   dboolean initialized;
 } exhud_component_t;
@@ -166,14 +167,16 @@ exhud_component_t components[exhud_component_count] = {
     dsda_DrawRenderStatsHC,
     dsda_EraseRenderStatsHC,
     "render_stats",
-    .strict = true
+    .strict = true,
+    .off_by_default = true
   },
   [exhud_fps] = {
     dsda_InitFPSHC,
     dsda_UpdateFPSHC,
     dsda_DrawFPSHC,
     dsda_EraseFPSHC,
-    "fps"
+    "fps",
+    .off_by_default = true
   },
 };
 
@@ -199,6 +202,11 @@ static void dsda_TurnComponentOff(int id) {
 static void dsda_InitializeComponent(int id, int x, int y, int vpt) {
   components[id].initialized = true;
   components[id].init(x, y, vpt | components[id].default_vpt | VPT_EX_TEXT);
+
+  if (components[id].off_by_default)
+    dsda_TurnComponentOff(id);
+  else
+    dsda_TurnComponentOn(id);
 }
 
 static char** dsda_HUDConfig(void) {
@@ -296,13 +304,13 @@ void dsda_InitExHud(void) {
 
       for (i = 0; i < exhud_component_count; ++i)
         if (!strncmp(command, components[i].name, sizeof(command))) {
-          int x, y, vpt, always_on;
+          int x, y, vpt;
           char alignment[16];
 
           found = true;
 
-          count = sscanf(args, "%d %d %15s %d", &x, &y, alignment, &always_on);
-          if (count != 4)
+          count = sscanf(args, "%d %d %15s", &x, &y, alignment);
+          if (count != 3)
             I_Error("Invalid hud component args \"%s\"", line);
 
           if (!strncmp(alignment, "bottom_left", sizeof(alignment)))
@@ -317,11 +325,6 @@ void dsda_InitExHud(void) {
             I_Error("Invalid hud component alignment \"%s\"", line);
 
           dsda_InitializeComponent(i, x, y, vpt);
-
-          if (always_on)
-            dsda_TurnComponentOn(i);
-          else
-            dsda_TurnComponentOff(i);
         }
 
       if (!found)
