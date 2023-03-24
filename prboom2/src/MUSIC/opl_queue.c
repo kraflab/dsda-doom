05750bfe3af0075456936dfbaa871efb54828f0e
Pierre Wendling <pierre.wendling.4@gmail.com>
2023-03-24 14:33:13 -0400

    Set Emacs style mode to C instead of C++.
    The C++ mode misleads GitHub into thinking this is a C++ project.

--- prboom2/src/MUSIC/opl_queue.c
+++ prboom2/src/MUSIC/opl_queue.c
@@ -1,4 +1,4 @@
-// Emacs style mode select   -*- C++ -*- 
+// Emacs style mode select   -*- C -*- 
 //-----------------------------------------------------------------------------
 //
 // Copyright(C) 2009 Simon Howard
