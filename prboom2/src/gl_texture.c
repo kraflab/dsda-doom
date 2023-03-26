3970c44da12484f6bf2e48e047ccd935e0bd53d7
kraflab <kraflab@gmail.com>
2023-03-26 21:25:07 +0200

    Remove unused gld_BlendOverTexture
    Co-Authored-By: Brian Koropoff <2101303+bkoropoff@users.noreply.github.com>

--- prboom2/src/gl_texture.c
+++ prboom2/src/gl_texture.c
@@ -924,28 +924,6 @@ unsigned char* gld_GetTextureBuffer(GLuint texid, int miplevel, int *width, int 
   return buf;
 }
 
-// e6y: from Quake3
-// R_BlendOverTexture
-// Apply a color blend over a set of pixels
-static void gld_BlendOverTexture(byte *data, int pixelCount, byte blend[4])
-{
-  int i;
-  int inverseAlpha;
-  int premult[3];
-
-  inverseAlpha = 255 - blend[3];
-  premult[0] = blend[0] * blend[3];
-  premult[1] = blend[1] * blend[3];
-  premult[2] = blend[2] * blend[3];
-
-  for(i = 0; i < pixelCount; i++, data += 4)
-  {
-    data[0] = (data[0] * inverseAlpha + premult[0]) >> 9;
-    data[1] = (data[1] * inverseAlpha + premult[1]) >> 9;
-    data[2] = (data[2] * inverseAlpha + premult[2]) >> 9;
-  }
-}
-
 void gld_SetTexFilters(GLTexture *gltexture)
 {
   int mag_filter, min_filter;
