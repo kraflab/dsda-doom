d350b5ee5f59e3c71cf6a5454f924b6760661e1a
Pierre Wendling <pierre.wendling.4@gmail.com>
2023-03-31 22:08:32 -0400

    zipfile: Apply suggestions from review.
    - Use a dsda string to manipulate paths.
    - Use I_Error to print and exit on error.
    - Read zip file by chunks to handle larger files.
    - Guard against unzipping more than 1GB.

--- prboom2/src/dsda/zipfile.c
+++ prboom2/src/dsda/zipfile.c
@@ -24,96 +24,71 @@
 
 #include "dsda/utility.h"
 
-static char *dsda_JoinPath(const char *prefix, const char *suffix) {
-  int joined_path_length;
-  char *joined_path;
+/* Allow a maximum of 1GB to be uncompressed to prevent zip-bombs */
+#define UNZIPPED_BYTES_LIMIT 1000000000ULL
+static zip_uint64_t total_bytes_read = 0;
 
-  joined_path_length = snprintf(NULL, 0, "%s%s", prefix, suffix) + 1;
-  joined_path = (char *)Z_Malloc(joined_path_length);
-  if (joined_path == NULL) {
-    lprintf(LO_ERROR, "dsda_JoinPath: Failed to allocate joined path.\n");
-    return NULL;
-  }
-  snprintf(joined_path, joined_path_length, "%s%s", prefix, suffix);
-  return joined_path;
-}
-
-static void dsda_CreateSubDir(const char *directory, const char *subdirectory) {
-  char *full_path;
-  int result;
-
-  full_path = dsda_JoinPath(directory, subdirectory);
-  if (full_path == NULL) {
-    return;
-  }
-  result = M_MakeDir(full_path, false);
-  Z_Free(full_path);
-  if (result != 0) {
-    lprintf(LO_ERROR, "dsda_CreateSubDir: Failed to create subdirectory %s in %s.\n", subdirectory, directory);
-  }
-}
-
-static FILE *dsda_CreateFile(const char *directory, const char *filename) {
-  FILE *file_handle;
-  char *full_path;
-
-  full_path = dsda_JoinPath(directory, filename);
-  if (full_path == NULL) {
-    return NULL;
-  }
-  file_handle = M_OpenFile(full_path, "wb");
-  Z_Free(full_path);
-  return file_handle;
-}
+#define CHUNK_SIZE 4 * 1024U
 
 static void dsda_WriteContentToFile(zip_file_t *input_file, FILE *dest_file, zip_uint64_t data_size) {
-  char *buffer;
+  byte buffer[CHUNK_SIZE];
 
-  buffer = (char *)Z_Malloc(data_size);
-  if (buffer == NULL) {
-    lprintf(LO_ERROR, "dsda_WriteContentToFile: Could not allocate temporary buffer.\n");
-    return;
+  while (data_size != 0) {
+    zip_uint64_t chunk_size;
+    zip_int64_t bytes_read;
+
+    chunk_size = MIN(data_size, CHUNK_SIZE);
+    bytes_read = zip_fread(input_file, buffer, chunk_size);
+    if (bytes_read == -1) {
+      I_Error("dsda_WriteContentToFile: Unable to read data from archive.\n");
+    }
+    if (fwrite(buffer, sizeof(char), bytes_read, dest_file) != bytes_read) {
+      I_Error("dsda_WriteContentToFile: Failed to write data to file.\n");
+    }
+    data_size -= bytes_read;
+    total_bytes_read += bytes_read;
+    if (total_bytes_read >= UNZIPPED_BYTES_LIMIT) {
+      I_Error("dsda_WriteContentToFile: Too much data to decompress.\n");
+    }
   }
-  zip_fread(input_file, buffer, data_size);
-  fwrite(buffer, sizeof(char), data_size, dest_file);
-  Z_Free(buffer);
 }
 
 static void dsda_WriteZippedFilesToDest(zip_t *archive, const char *destination_directory) {
   for (zip_int64_t i = 0; i < zip_get_num_entries(archive, ZIP_FL_UNCHANGED); i++) {
+    dsda_string_t full_path;
     zip_file_t *zipped_file;
     zip_stat_t stat;
-    FILE *dest_file_handle;
+    FILE *dest_file;
     const char *file_name = zip_get_name(archive, i, ZIP_FL_UNCHANGED);
 
-    /* Intermediate directories show up as file */
-    if (dsda_HasFileExt(file_name, "/")) {
-      dsda_CreateSubDir(destination_directory, file_name);
+    dsda_StringPrintF(&full_path, "%s%s", destination_directory, file_name);
+
+    /* Intermediate directories have a trailing '/' */
+    if (dsda_HasFileExt(full_path.string, "/")) {
+      M_MakeDir(full_path.string, true);
+      dsda_FreeString(&full_path);
       continue;
     }
 
     zip_stat_index(archive, i, ZIP_FL_UNCHANGED, &stat);
     if ((stat.valid & ZIP_STAT_SIZE) == 0) {
-      lprintf(LO_ERROR, "dsda_WriteZippedFilesToDest: Failed to read size of zipped file %s.\n", file_name);
-      return;
+      I_Error("dsda_WriteZippedFilesToDest: Failed to read size of zipped file %s.\n", file_name);
     }
 
     zipped_file = zip_fopen_index(archive, i, ZIP_FL_UNCHANGED);
     if (zipped_file == NULL) {
-      lprintf(LO_ERROR, "dsda_WriteZippedFilesToDest: Failed to open zipped file %s.\n", file_name);
-      return;
+      I_Error("dsda_WriteZippedFilesToDest: Failed to open zipped file %s.\n", file_name);
     }
 
-    dest_file_handle = dsda_CreateFile(destination_directory, file_name);
-    if (dest_file_handle == NULL) {
-      lprintf(LO_ERROR, "dsda_WriteZippedFilesToDest: Failed to create destination file %s%s.\n", destination_directory,
-              file_name);
-      zip_fclose(zipped_file);
-      return;
+    dest_file = M_OpenFile(full_path.string, "wb");
+    if (dest_file == NULL) {
+      I_Error("dsda_WriteZippedFilesToDest: Failed to open destination file %s.\n", full_path.string);
     }
-    dsda_WriteContentToFile(zipped_file, dest_file_handle, stat.size);
+
+    dsda_WriteContentToFile(zipped_file, dest_file, stat.size);
     zip_fclose(zipped_file);
-    fclose(dest_file_handle);
+    fclose(dest_file);
+    dsda_FreeString(&full_path);
   }
 }
 
@@ -125,9 +100,7 @@ void dsda_UnzipFile(const char *zipped_file_name, const char *destination_direct
   if (archive_handle == NULL) {
     zip_error_t error;
     zip_error_init_with_code(&error, error_code);
-    lprintf(LO_ERROR, "dsda_UnzipFile: Unable to open %s: %s.\n", zipped_file_name, zip_error_strerror(&error));
-    zip_error_fini(&error);
-    return;
+    I_Error("dsda_UnzipFile: Unable to open %s: %s.\n", zipped_file_name, zip_error_strerror(&error));
   }
   dsda_WriteZippedFilesToDest(archive_handle, destination_directory);
   zip_close(archive_handle);
