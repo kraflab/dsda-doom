beb1bc472b58f92681c12210f6780aa8f20b0abf
kraflab <kraflab@gmail.com>
2023-05-05 20:21:23 +0200

    Remove unused WI_Responder
    Co-Authored-By: Brian Koropoff <2101303+bkoropoff@users.noreply.github.com>

--- prboom2/src/wi_stuff.c
+++ prboom2/src/wi_stuff.c
@@ -436,20 +436,6 @@ static void WI_slamBackground(void)
   V_DrawNamePatch(0, 0, FB, name, CR_DEFAULT, VPT_STRETCH);
 }
 
-
-// ====================================================================
-// WI_Responder
-// Purpose: Draw animations on intermission background screen
-// Args:    ev    -- event pointer, not actually used here.
-// Returns: False -- dummy routine
-//
-// The ticker is used to detect keys
-//  because of timing issues in netgames.
-dboolean WI_Responder(event_t* ev)
-{
-  return false;
-}
-
 #define SPACEWIDTH 4
 
 static void WI_DrawString(int cx, int cy, const char* ch)
