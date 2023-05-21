4fe7577f8da827fc9afd8c89c105ab1bfe636bc6
Brian Koropoff <bkoropoff@gmail.com>
2023-05-21 13:42:32 -0700

    Revert "Protect against out of bounds finetangent"
    This reverts commit 54b0215d8ef5d71050821b049baf10c118bc8527.

--- prboom2/src/r_segs.c
+++ prboom2/src/r_segs.c
@@ -541,9 +541,6 @@ static void R_RenderSegLoop (void)
       // calculate texture offset
       angle_t angle =(rw_centerangle+xtoviewangle[rw_x])>>ANGLETOFINESHIFT;
 
-      if (angle >= 4096)
-        return;
-
       texturecolumn = rw_offset-FixedMul(finetangent[angle],rw_distance);
       texturecolumn >>= FRACBITS;
 
