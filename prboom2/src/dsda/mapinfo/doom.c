dd87220c2c2d6bc7090c12f8106a4c54ec15c967
kraflab <kraflab@gmail.com>
2023-06-24 22:11:47 +0200

    Add empty doom mapinfo stub

---
+++ prboom2/src/dsda/mapinfo/doom.c
@@ -0,0 +1,154 @@
+//
+// Copyright(C) 2023 by Ryan Krafnick
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// DESCRIPTION:
+//  DSDA MapInfo Doom
+//
+
+#include "doom.h"
+
+int dsda_DoomFirstMap(int* episode, int* map) {
+  return false; // TODO
+}
+
+int dsda_DoomNewGameMap(int* episode, int* map) {
+  return false; // TODO
+}
+
+int dsda_DoomResolveWarp(int* args, int arg_count, int* episode, int* map) {
+  return false; // TODO
+}
+
+int dsda_DoomNextMap(int* episode, int* map) {
+  return false; // TODO
+}
+
+int dsda_DoomShowNextLocBehaviour(int* behaviour) {
+  return false; // TODO
+}
+
+int dsda_DoomSkipDrawShowNextLoc(int* skip) {
+  return false; // TODO
+}
+
+void dsda_DoomUpdateMapInfo(void) {
+  // TODO
+}
+
+void dsda_DoomUpdateLastMapInfo(void) {
+  // TODO
+}
+
+void dsda_DoomUpdateNextMapInfo(void) {
+  // TODO
+}
+
+int dsda_DoomResolveCLEV(int* clev, int* episode, int* map) {
+  return false; // TODO
+}
+
+int dsda_DoomResolveINIT(int* init) {
+  return false; // TODO
+}
+
+int dsda_DoomMusicIndexToLumpNum(int* lump, int music_index) {
+  return false; // TODO
+}
+
+int dsda_DoomMapMusic(int* music_index, int* music_lump) {
+  return false; // TODO
+}
+
+int dsda_DoomInterMusic(int* music_index, int* music_lump) {
+  return false; // TODO
+}
+
+int dsda_DoomStartFinale(void) {
+  return false; // TODO
+}
+
+int dsda_DoomFTicker(void) {
+  return false; // TODO
+}
+
+void dsda_DoomFDrawer(void) {
+  return; // TODO
+}
+
+int dsda_DoomBossAction(mobj_t* mo) {
+  return false; // TODO
+}
+
+int dsda_DoomHUTitle(dsda_string_t* str) {
+  return false; // TODO
+}
+
+int dsda_DoomSkyTexture(int* sky) {
+  return false; // TODO
+}
+
+int dsda_DoomPrepareInitNew(void) {
+  return false; // TODO
+}
+
+int dsda_DoomPrepareIntermission(int* result) {
+  return false; // TODO
+}
+
+int dsda_DoomPrepareFinale(int* result) {
+  return false; // TODO
+}
+
+void dsda_DoomLoadMapInfo(void) {
+  return; // TODO
+}
+
+int dsda_DoomExitPic(const char** exit_pic) {
+  return false; // TODO
+}
+
+int dsda_DoomEnterPic(const char** enter_pic) {
+  return false; // TODO
+}
+
+int dsda_DoomPrepareEntering(void) {
+  return false; // TODO
+}
+
+int dsda_DoomPrepareFinished(void) {
+  return false; // TODO
+}
+
+int dsda_DoomMapLightning(int* lightning) {
+  return false; // TODO
+}
+
+int dsda_DoomApplyFadeTable(void) {
+  return false; // TODO
+}
+
+int dsda_DoomMapCluster(int* cluster, int map) {
+  return false; // TODO
+}
+
+int dsda_DoomSky1Texture(short* texture) {
+  return false; // TODO
+}
+
+int dsda_DoomSky2Texture(short* texture) {
+  return false; // TODO
+}
+
+int dsda_DoomInitSky(void) {
+  return false; // TODO
+}
