5ebf67946f5a707d5200d5147980d81b28b11c31
Arsinikk <me@andrikpowell.com>
2025-02-13 21:23:26 -0600

    Refresh stbar when changing view size

--- prboom2/src/r_main.c
+++ prboom2/src/r_main.c
@@ -500,11 +500,13 @@ static void R_InitLightTables (void)
 // The change will take effect next refresh.
 //
 
+extern dboolean BorderNeedRefresh;
 dboolean setsizeneeded;
 static int setblocks;
 
 void R_SetViewSize(void)
 {
+  BorderNeedRefresh = true;
   setsizeneeded = true;
   setblocks = dsda_IntConfig(dsda_config_screenblocks);
 }
