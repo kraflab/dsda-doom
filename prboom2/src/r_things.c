9a378c94d1fbd20a50048daab383af97d704cf5d
Arsinikk <me@andrikpowell.com>
2025-02-16 04:44:20 -0600

    Fix Weapon Alignment For Custom Dehacked States

--- prboom2/src/r_things.c
+++ prboom2/src/r_things.c
@@ -1061,21 +1061,7 @@ static void R_DrawPSprite (pspdef_t *psp)
   flip = (dboolean)(sprframe->flip & 1);
 
   {
-    weaponinfo_t *winfo;
-    int state;
-    int weapon_attack_alignment;
-
-    if (hexen)
-    {
-      winfo = &hexen_weaponinfo[viewplayer->readyweapon][viewplayer->pclass];
-    }
-    else
-    {
-      winfo = &weaponinfo[viewplayer->readyweapon];
-    }
-
-    state = viewplayer->psprites[ps_weapon].state - states;
-    weapon_attack_alignment = dsda_IntConfig(dsda_config_weapon_attack_alignment);
+    int weapon_attack_alignment = dsda_IntConfig(dsda_config_weapon_attack_alignment);
 
     if (!dsda_WeaponBob())
     {
@@ -1083,12 +1069,12 @@ static void R_DrawPSprite (pspdef_t *psp)
 
       psp_sx = FRACUNIT;
 
-      if (state != winfo->downstate && state != winfo->upstate)
+      if (psp->state->action != A_Lower && psp->state->action != A_Raise)
       {
         last_sy = psp->sy;
         psp_sy = 32 * FRACUNIT;
       }
-      else if (state == winfo->downstate)
+      else if (psp->state->action == A_Lower)
       {
         // We want to move smoothly from where we were
         psp_sy -= (last_sy - 32 * FRACUNIT);
@@ -1100,7 +1086,7 @@ static void R_DrawPSprite (pspdef_t *psp)
 
       // [crispy] don't center vertically during lowering and raising states
       if (weapon_attack_alignment >= CENTERWEAPON_HORVER &&
-          state != winfo->downstate && state != winfo->upstate)
+          psp->state->action != A_Lower && psp->state->action != A_Raise)
       {
           R_ApplyWeaponBob(NULL, false, &psp_sy, weapon_attack_alignment == CENTERWEAPON_BOB);
       }
