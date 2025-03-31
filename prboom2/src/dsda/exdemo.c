kraflab/dsda-doom

commit 0d35e89640f96a32f404ddf287a0cd42a63f39a0

Expand features bit array size to 128 (#613)

prboom2/src/dsda/exdemo.c
@@ -42,7 +42,7 @@ typedef struct {
   byte* footer;
   size_t demo_size;
   size_t footer_size;
-  uint64_t features;
+  byte features[FEATURE_SLOTS];
   int is_signed;
 } exdemo_t;
 
@@ -401,21 +401,36 @@ void dsda_MergeExDemoFeatures(void) {
 static void DemoEx_GetFeatures(const wadinfo_t* header) {
   char* str;
   char signature[33];
+  char ftext[2 * FEATURE_SLOTS + 1];
+  int ftext_start = 0;
+  int ftext_end = 0;
 
   exdemo.is_signed = 0;
-  exdemo.features = 0;
+  for (int f = 0; f < FEATURE_SLOTS; f++) {
+    exdemo.features[f] = 0;
+  }
 
   str = DemoEx_LumpAsString(DEMOEX_FEATURE_LUMPNAME, header);
   if (!str)
     return;
 
-  if (sscanf(str, "%*[^\n]\n%" PRIx64 "-%32s", &exdemo.features, signature) == 2) {
-    byte features[FEATURE_SIZE];
+  if (sscanf(str, "%*[^\n]\n0x%n%[^-]%n-%32s", &ftext_start, ftext, &ftext_end, signature) == 2) {
     dsda_cksum_t cksum;
+    char padded_ftext[2 * FEATURE_SLOTS + 1];
+    int i;
 
-    dsda_CopyFeatures2(features, exdemo.features);
+    for (i = 0; i < (2 * FEATURE_SLOTS) - (ftext_end - ftext_start); i++)
+      strcat(padded_ftext, "0");
+    strcat(padded_ftext, ftext);
 
-    dsda_GetDemoCheckSum(&cksum, features, exdemo.demo, exdemo.demo_size);
+    for (int f = 0; f < FEATURE_SLOTS; f++) {
+      char current_text[3];
+      strncpy(current_text, padded_ftext + f * 2, 2);
+      current_text[2] = '\0';
+      exdemo.features[FEATURE_SLOTS - f - 1] = strtol(current_text, NULL, 16);
+    }
+
+    dsda_GetDemoCheckSum(&cksum, exdemo.features, exdemo.demo, exdemo.demo_size);
 
     if (!strcmp(signature, cksum.string))
       exdemo.is_signed = 1;
@@ -433,17 +448,27 @@ static void DemoEx_AddFeatures(wadtbl_t* wadtbl) {
   char* description;
   byte* buffer;
   size_t buffer_length;
-  uint64_t features;
+  byte *features;
+  char current_feature[3];
 
   dsda_GetDemoRecordingCheckSum(&cksum);
   description = dsda_DescribeFeatures();
   features = dsda_UsedFeatures();
 
-  // 18 for 64 bits in hex + \n + \0 + \- + extra space :^)
-  buffer_length = strlen(cksum.string) + strlen(description) + 24;
+  // (2 + 2 * FEATURE_SLOTS) for the bitmap size in hex + \n + \0 + \- + extra space :^)
+  buffer_length = strlen(cksum.string) + strlen(description) + 8 + 2 * FEATURE_SLOTS;
   buffer = Z_Calloc(buffer_length, 1);
 
-  snprintf(buffer, buffer_length, "%s\n0x%016" PRIx64 "-%s", description, features, cksum.string);
+  strcpy(buffer, description);
+  strcat(buffer, "\n0x");
+
+  for (int f = 0; f < FEATURE_SLOTS; f++) {
+    snprintf(current_feature, 3, "%02" PRIx8, features[FEATURE_SLOTS - f - 1]);
+    strncat(buffer, current_feature, 2);
+  }
+
+  strcat(buffer, "-");
+  strcat(buffer, cksum.string);
 
   AddPWADTableLump(wadtbl, DEMOEX_FEATURE_LUMPNAME, buffer, buffer_length);
 

prboom2/src/dsda/features.c
@@ -23,9 +23,9 @@
 
 #include "features.h"
 
-static uint64_t used_features;
+static byte used_features[FEATURE_SLOTS];
 
-static const char* feature_names[64] = {
+static const char* feature_names[FEATURE_SIZE] = {
   [uf_menu] = "Menu",
   [uf_exhud] = "Extended HUD",
   [uf_advhud] = "Advanced HUD",
@@ -67,37 +67,28 @@ static const char* feature_names[64] = {
   [uf_advanced_map] = "Advanced Map",
 };
 
-#define FEATURE_BIT(x) ((uint64_t) 1 << x)
-
 void dsda_TrackFeature(int feature) {
-  used_features |= FEATURE_BIT(feature);
+  BITSET(used_features, feature);
 }
 
 void dsda_ResetFeatures(void) {
-  used_features = 0;
+  memset(used_features, 0, FEATURE_SLOTS);
 }
 
-uint64_t dsda_UsedFeatures(void) {
+byte *dsda_UsedFeatures(void) {
   return used_features;
 }
 
-void dsda_MergeFeatures(uint64_t source) {
-  used_features |= source;
+void dsda_MergeFeatures(byte *source) {
+  for (int f = 0; f < FEATURE_SLOTS; f++) {
+    used_features[f] |= source[f];
+  }
 }
 
 void dsda_CopyFeatures(byte* result) {
-  dsda_CopyFeatures2(result, used_features);
-}
-
-void dsda_CopyFeatures2(byte* result, uint64_t source) {
-  result[0] = (source      ) & 0xff;
-  result[1] = (source >>  8) & 0xff;
-  result[2] = (source >> 16) & 0xff;
-  result[3] = (source >> 24) & 0xff;
-  result[4] = (source >> 32) & 0xff;
-  result[5] = (source >> 40) & 0xff;
-  result[6] = (source >> 48) & 0xff;
-  result[7] = (source >> 56) & 0xff;
+  for (int f = 0; f < FEATURE_SLOTS; f++) {
+    result[f] = used_features[f];
+  }
 }
 
 char* dsda_DescribeFeatures(void) {
@@ -107,8 +98,8 @@ char* dsda_DescribeFeatures(void) {
 
   dsda_InitString(&description, NULL);
 
-  for (i = 0; i < 64; ++i)
-    if (used_features & FEATURE_BIT(i) && feature_names[i]) {
+  for (int i = 0; i < FEATURE_SIZE; i++) {
+    if (BITTEST(used_features, i) && feature_names[i]) {
       if (first)
         first = false;
       else
@@ -116,6 +107,7 @@ char* dsda_DescribeFeatures(void) {
 
       dsda_StringCat(&description, feature_names[i]);
     }
+  }
 
   if (!description.string)
     dsda_StringCat(&description, "Tachyeres pteneres");

prboom2/src/dsda/demo.c
@@ -449,7 +449,7 @@ void dsda_GetDemoCheckSum(dsda_cksum_t* cksum, byte* features, byte* demo, size_
 
   MD5Update(&md5, demo, demo_size);
 
-  MD5Update(&md5, features, FEATURE_SIZE);
+  MD5Update(&md5, features, FEATURE_SLOTS);
 
   MD5Final(cksum->bytes, &md5);
 
@@ -457,7 +457,7 @@ void dsda_GetDemoCheckSum(dsda_cksum_t* cksum, byte* features, byte* demo, size_
 }
 
 void dsda_GetDemoRecordingCheckSum(dsda_cksum_t* cksum) {
-  byte features[FEATURE_SIZE];
+  byte features[FEATURE_SLOTS];
 
   dsda_CopyFeatures(features);
 

prboom2/src/dsda/features.h
@@ -86,12 +86,20 @@ typedef enum {
   // 63
 } dsda_feature_flag_t;
 
-#define FEATURE_SIZE 8
+#define BITMASK(b) (1 << ((b) % 8))
+#define BITSLOT(b) ((b) / 8)
+#define BITSET(a, b) ((a)[BITSLOT(b)] |= BITMASK(b))
+#define BITCLEAR(a, b) ((a)[BITSLOT(b)] &= ~BITMASK(b))
+#define BITTEST(a, b) ((a)[BITSLOT(b)] & BITMASK(b))
+#define BITNSLOTS(nb) ((nb + 8 - 1) / 8)
+
+#define FEATURE_SIZE 128
+#define FEATURE_SLOTS BITNSLOTS(FEATURE_SIZE)
+
 
 void dsda_TrackFeature(int feature);
 void dsda_ResetFeatures(void);
-uint64_t dsda_UsedFeatures(void);
-void dsda_MergeFeatures(uint64_t source);
+byte *dsda_UsedFeatures(void);
+void dsda_MergeFeatures(byte *source);
 void dsda_CopyFeatures(byte* result);
-void dsda_CopyFeatures2(byte* result, uint64_t source);
 char* dsda_DescribeFeatures(void);

prboom2/src/dsda/save.c
@@ -43,17 +43,16 @@ extern int dsda_max_kill_requirement;
 extern int player_damage_last_tic;
 
 static void dsda_ArchiveInternal(void) {
-  uint64_t features;
-
   P_SAVE_X(dsda_max_kill_requirement);
   P_SAVE_X(player_damage_last_tic);
 
-  features = dsda_UsedFeatures();
-  P_SAVE_X(features);
+  for (int f = 0; f < FEATURE_SLOTS; f++) {
+    P_SAVE_X(dsda_UsedFeatures()[f]);
+  }
 }
 
 static void dsda_UnArchiveInternal(void) {
-  uint64_t features;
+  byte features[FEATURE_SLOTS];
 
   P_LOAD_X(dsda_max_kill_requirement);
   P_LOAD_X(player_damage_last_tic);

prboom2/src/p_saveg.h
@@ -36,7 +36,7 @@
 
 #include "doomtype.h"
 
-#define SAVEVERSION 5
+#define SAVEVERSION 6
 
 /* Persistent storage/archiving.
  * These are the load / save game routines. */

commit d41c0a9373f810a326636bcf7deb9e945d05bc25

Fix crash when reading demo footer
Crash introduced in 0d35e89640f96a32f404ddf287a0cd42a63f39a0

prboom2/src/dsda/exdemo.c
@@ -419,9 +419,10 @@ static void DemoEx_GetFeatures(const wadinfo_t* header) {
     char padded_ftext[2 * FEATURE_SLOTS + 1];
     int i;
 
+    memset(&padded_ftext, 0, sizeof(padded_ftext));
     for (i = 0; i < (2 * FEATURE_SLOTS) - (ftext_end - ftext_start); i++)
       strcat(padded_ftext, "0");
-    strcat(padded_ftext, ftext);
+    strncat(padded_ftext, ftext, ftext_end - ftext_start);
 
     for (int f = 0; f < FEATURE_SLOTS; f++) {
       char current_text[3];

prboom2/src/dsda/features.c
@@ -98,7 +98,7 @@ char* dsda_DescribeFeatures(void) {
 
   dsda_InitString(&description, NULL);
 
-  for (int i = 0; i < FEATURE_SIZE; i++) {
+  for (i = 0; i < FEATURE_SIZE; i++) {
     if (BITTEST(used_features, i) && feature_names[i]) {
       if (first)
         first = false;