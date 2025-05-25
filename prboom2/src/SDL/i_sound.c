be29fce07be89afc7dc33ea4297b58e4d2b5a0d2
Kacper Słomiński <kacper.slominski72@gmail.com>
2025-05-25 14:40:13 +0200

    Add a tracker music player based on libopenmpt
    Intended to be a replacement for the DUMB-based tracker player.
    
    Bug: #487

--- prboom2/src/SDL/i_sound.c
+++ prboom2/src/SDL/i_sound.c
@@ -866,6 +866,7 @@ static void PlaySong(int handle, int looping);
 #include "MUSIC/flplayer.h"
 #include "MUSIC/vorbisplayer.h"
 #include "MUSIC/portmidiplayer.h"
+#include "MUSIC/libopenmptplayer.h"
 
 static Mix_Music *music[2] = { NULL, NULL };
 
@@ -883,6 +884,7 @@ static const music_player_t *music_players[] =
   &vorb_player, // vorbisplayer.h
   &mp_player, // madplayer.h
   &db_player, // dumbplayer.h
+  &mpt_player, // libopenmptplayer.h
   &fl_player, // flplayer.h
   &opl_synth_player, // oplplayer.h
   &pm_player, // portmidiplayer.h
@@ -895,6 +897,7 @@ static int music_player_was_init[NUM_MUS_PLAYERS];
 #define PLAYER_VORBIS     "vorbis player"
 #define PLAYER_MAD        "mad mp3 player"
 #define PLAYER_DUMB       "dumb tracker player"
+#define PLAYER_LIBOPENMPT "libopenmpt tracker player"
 #define PLAYER_FLUIDSYNTH "fluidsynth midi player"
 #define PLAYER_OPL        "opl synth player"
 #define PLAYER_PORTMIDI   "portmidi midi player"
@@ -905,6 +908,7 @@ char music_player_order[NUM_MUS_PLAYERS][200] =
   PLAYER_VORBIS,
   PLAYER_MAD,
   PLAYER_DUMB,
+  PLAYER_LIBOPENMPT,
   PLAYER_FLUIDSYNTH,
   PLAYER_OPL,
   PLAYER_PORTMIDI,
@@ -1336,21 +1340,21 @@ void M_ChangeMIDIPlayer(void)
 
   if (!strcasecmp(snd_midiplayer, midiplayers[midi_player_fluidsynth]))
   {
-    strcpy(music_player_order[3], PLAYER_FLUIDSYNTH);
-    strcpy(music_player_order[4], PLAYER_OPL);
-    strcpy(music_player_order[5], PLAYER_PORTMIDI);
+    strcpy(music_player_order[4], PLAYER_FLUIDSYNTH);
+    strcpy(music_player_order[5], PLAYER_OPL);
+    strcpy(music_player_order[6], PLAYER_PORTMIDI);
   }
   else if (!strcasecmp(snd_midiplayer, midiplayers[midi_player_opl]))
   {
-    strcpy(music_player_order[3], PLAYER_OPL);
-    strcpy(music_player_order[4], PLAYER_FLUIDSYNTH);
-    strcpy(music_player_order[5], PLAYER_PORTMIDI);
+    strcpy(music_player_order[4], PLAYER_OPL);
+    strcpy(music_player_order[5], PLAYER_FLUIDSYNTH);
+    strcpy(music_player_order[6], PLAYER_PORTMIDI);
   }
   else if (!strcasecmp(snd_midiplayer, midiplayers[midi_player_portmidi]))
   {
-    strcpy(music_player_order[3], PLAYER_PORTMIDI);
-    strcpy(music_player_order[4], PLAYER_FLUIDSYNTH);
-    strcpy(music_player_order[5], PLAYER_OPL);
+    strcpy(music_player_order[4], PLAYER_PORTMIDI);
+    strcpy(music_player_order[5], PLAYER_FLUIDSYNTH);
+    strcpy(music_player_order[6], PLAYER_OPL);
   }
 
   S_StopMusic();
