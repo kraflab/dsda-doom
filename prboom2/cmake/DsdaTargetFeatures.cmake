bbbb6e7ab64de9ca434b450c1d68aa4a15ae93e9
Pierre Wendling <pierre.wendling.4@gmail.com>
2025-06-26 10:10:23 +0200

    CMake: Move config.h generation to its own module.
    This unclutters the top CMakeLists.txt and moves all the target
    settings needed for using config.h to the same place.
    
    Also check that all the needed variables are set before generating the
    file.

--- prboom2/cmake/DsdaTargetFeatures.cmake
+++ prboom2/cmake/DsdaTargetFeatures.cmake
@@ -1,12 +1,8 @@
 include_guard()
 
-# Internal functions, these should not be called from outside this module
+include(DsdaHelpers)
 
-function(dsda_internal_fail_if_invalid_target tgt)
-  if(NOT TARGET ${tgt})
-    message(FATAL_ERROR "${tgt} is not a valid CMake target.")
-  endif()
-endfunction()
+# Internal functions, these should not be called from outside this module
 
 function(dsda_internal_setup_warnings_msvc result_var)
   set(${result_var}
@@ -102,7 +98,7 @@ endfunction()
 # Public functions
 
 function(dsda_target_set_warnings tgt)
-  dsda_internal_fail_if_invalid_target(${tgt})
+  dsda_fail_if_invalid_target(${tgt})
   
   if(NOT DEFINED CACHE{DSDA_ENABLED_WARNINGS})
     dsda_internal_setup_warnings()
@@ -115,7 +111,7 @@ function(dsda_target_set_warnings tgt)
 endfunction()
 
 function(dsda_target_silence_deprecation tgt)
-  dsda_internal_fail_if_invalid_target(${tgt})
+  dsda_fail_if_invalid_target(${tgt})
 
   if(WIN32)
     target_compile_definitions(${tgt}
@@ -132,7 +128,7 @@ function(dsda_target_silence_deprecation tgt)
 endfunction()
 
 function(dsda_target_enable_fast_math tgt)
-  dsda_internal_fail_if_invalid_target(${tgt})
+  dsda_fail_if_invalid_target(${tgt})
 
   if(NOT DEFINED CACHE{DSDA_FAST_MATH_FLAG})
     dsda_internal_check_fast_math_flag()
