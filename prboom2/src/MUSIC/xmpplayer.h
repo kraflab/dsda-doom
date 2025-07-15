f900fd2aa47f4fb32ceeea740a091dd2e608837c
ceski <56656010+ceski-1@users.noreply.github.com>
2025-07-14 17:24:11 -0700

    Switch to libxmp for tracker music

---
+++ prboom2/src/MUSIC/xmpplayer.h
@@ -0,0 +1,22 @@
+//
+// Copyright(C) 2025 ceski
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// DESCRIPTION:
+//
+
+#ifndef XMPPLAYER_H
+#define XMPPLAYER_H
+
+extern const music_player_t xmp_player;
+
+#endif // XMPPLAYER_H
