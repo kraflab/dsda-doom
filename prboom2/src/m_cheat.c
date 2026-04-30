0f9a5a4ec4a6c5359337bcfd11000453a59b99ce
nathan-casabieille <nathan.casabieille1@gmail.com>
2026-04-30 17:17:11 +0200

    Fix tntfast cheat not toggling fast monsters

--- prboom2/src/m_cheat.c
+++ prboom2/src/m_cheat.c
@@ -815,8 +815,8 @@ static void cheat_hom()
 // killough 3/6/98: -fast parameter toggle
 static void cheat_fast()
 {
-  dsda_AddMessage((fastparm = !fastparm) ? "Fast Monsters On" : "Fast Monsters Off");
-  dsda_RefreshGameSkill(); // refresh fast monsters
+  dsda_AddMessage(dsda_ToggleConfig(dsda_config_fast_monsters, true) ? "Fast Monsters On" : "Fast Monsters Off");
+  dsda_RefreshGameSkill();
 }
 
 // killough 2/16/98: keycard/skullkey cheat functions
