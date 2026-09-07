a1b0df1c0d259960cf57d1225faee5b2930935fc
Arsinikk <61759217+andrikpowell@users.noreply.github.com>
2026-09-07 06:58:46 -0500

    Minor fix

--- prboom2/src/dsda/endoom.c
+++ prboom2/src/dsda/endoom.c
@@ -383,7 +383,7 @@ void dsda_CacheEndoom(void) {
       lump = W_CheckNumForName("ENDOOM");
   }
 
-  pwad_only = (show_endoom==2 && !W_PWADLumpNumExists(lump) && W_PWADMapExists());
+  pwad_only = (show_endoom==2 && !W_PWADLumpNumExists(lump) && W_PWADMapsExist());
 
   if (lump == LUMP_NOT_FOUND || W_LumpLength(lump) != 4000 || pwad_only)
     return;
