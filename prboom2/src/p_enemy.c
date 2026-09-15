87e5c7f8fa3af617658c41429dee66b98fe6402c
Arsinikk <me@andrikpowell.com>
2026-09-15 11:57:03 -0500

    Add missing `dsda_WatchFailedSpawn()` - for Hexen stats

--- prboom2/src/p_enemy.c
+++ prboom2/src/p_enemy.c
@@ -4215,6 +4215,7 @@ void A_GenWizard(mobj_t * actor)
     dsda_WatchDSparilSpawn(mo);
     if (P_TestMobjLocation(mo) == false)
     {                           // Didn't fit
+        dsda_WatchFailedSpawn(mo);
         P_RemoveMobj(mo);
         return;
     }
@@ -8005,6 +8006,7 @@ void A_SpawnBishop(mobj_t * actor)
     {
         if (!P_TestMobjLocation(mo))
         {
+            dsda_WatchFailedSpawn(mo);
             P_SetMobjState(mo, HEXEN_S_NULL);
         }
     }
